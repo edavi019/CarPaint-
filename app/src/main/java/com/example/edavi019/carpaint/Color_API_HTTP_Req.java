f1a26cd25300d3c072d9d2587ec53e7a1fe5566c
James Minckler <jminc005@plattsburgh.edu>
2019-02-28 18:42:09 -0500

    Created Main activity + color api page

---
+++ app/src/main/java/com/example/edavi019/carpaint/Color_API_HTTP_Req.java
@@ -0,0 +1,13 @@
+package com.example.edavi019.carpaint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Color_API_HTTP_Req extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color__api__http__req);
+    }
+}
